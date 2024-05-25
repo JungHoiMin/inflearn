635ecbff4d676d01b8e3a87579a7fb4b68a0d425
정회민 <wony1102@naver.com>
2024-05-25 17:49:19 +0900

    Section 2 Vue Router v4.x - Vue Router란?: 프로젝트 적용

--- Vue3 완벽 마스터_기초부터 실전까지(실전편)/vue3-posts/src/main.js
+++ Vue3 완벽 마스터_기초부터 실전까지(실전편)/vue3-posts/src/main.js
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import router from '@/router/index.js';
 
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.js';
