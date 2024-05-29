8cdd2a0d804a3f128dbb9be7062f4f3ef276f9c4
정회민 <wony1102@naver.com>
2024-05-29 12:50:56 +0900

    Section 3 HTTP API 통신 - Pagination & Filter 구현하기

--- Vue3 완벽 마스터_기초부터 실전까지(실전편)/vue3-posts/src/api/posts.js
+++ Vue3 완벽 마스터_기초부터 실전까지(실전편)/vue3-posts/src/api/posts.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const getPosts = () => {
-  return axios.get('http://localhost:5000/posts');
+export const getPosts = params => {
+  return axios.get('http://localhost:5000/posts', { params });
 };
 
 export const getPostById = id => {
